e44ce433b90fc0cd853de148d5bb85479f3d2682
Rudyrue <73214127+Rudyrue@users.noreply.github.com>
2024-12-12 15:20:24 -0700

    Fix actions
    ok HOPEFULLY this should fix actions
    
    i KNEW it was gonna do that FUCK
    
    oops
    
    b
    
    ok this should fix the hxcpp thing
    
    i love github yipe
    
    iuph
    
    ok linux might work
    
    ok MAYBE this time it'll work
    
    hopefully this'll work
    
    maybe if i just
    
    make it not bloat the cli in workflows
    
    p
    
    ok this SHOULD fix windows
    
    maybe
    
    oops 2
    
    TEMP - fast download for hxcpp
    
    Update main.yml
    
    Update main.yml
    
    Small changes
    
    Update build-hxcpp.bat
    
    Okay this should work

--- _setup/windows.bat
+++ _setup/windows.bat
@@ -4,19 +4,19 @@ cd ..
 @echo on
 echo Installing dependencies...
 echo This might take a few moments depending on your internet speed.
-haxelib install lime 8.1.2
-haxelib install openfl 9.3.3
-haxelib install flixel 5.5.0
-haxelib install flixel-addons 3.2.2
-haxelib install flixel-tools 1.5.1
-haxelib install hscript-iris 1.1.0
-haxelib install hxdiscord_rpc 1.2.4
-haxelib install hxvlc 1.9.2
-haxelib install hxjson5
-haxelib git hxcpp https://github.com/HaxeFoundation/hxcpp 54af892be2ca4c63988c99c9c524431af6c6f036
-haxelib git flxanimate https://github.com/Dot-Stuff/flxanimate 768740a56b26aa0c072720e0d1236b94afe68e3e
-haxelib git linc_luajit https://github.com/superpowers04/linc_luajit 1906c4a96f6bb6df66562b3f24c62f4c5bba14a7
-haxelib git funkin.vis https://github.com/FunkinCrew/funkVis 22b1ce089dd924f15cdc4632397ef3504d464e90
-haxelib git grig.audio https://gitlab.com/haxe-grig/grig.audio.git cbf91e2180fd2e374924fe74844086aab7891666
+haxelib install lime 8.1.2 --quiet
+haxelib install openfl 9.3.3 --quiet
+haxelib install flixel 5.5.0 --quiet
+haxelib install flixel-addons 3.2.2 --quiet
+haxelib install flixel-tools 1.5.1 --quiet
+haxelib install hscript-iris 1.1.0 --quiet
+haxelib install hxdiscord_rpc 1.2.4 --quiet --skip-dependencies
+haxelib install hxvlc 1.9.2 --quiet --skip-dependencies
+haxelib install hxjson5 --quiet
+haxelib git hxcpp https://github.com/HaxeFoundation/hxcpp 54af892be2ca4c63988c99c9c524431af6c6f036 --quiet
+haxelib git flxanimate https://github.com/Dot-Stuff/flxanimate 768740a56b26aa0c072720e0d1236b94afe68e3e --quiet
+haxelib git linc_luajit https://github.com/superpowers04/linc_luajit 1906c4a96f6bb6df66562b3f24c62f4c5bba14a7 --quiet
+haxelib git funkin.vis https://github.com/FunkinCrew/funkVis 22b1ce089dd924f15cdc4632397ef3504d464e90 --quiet
+haxelib git grig.audio https://gitlab.com/haxe-grig/grig.audio.git cbf91e2180fd2e374924fe74844086aab7891666 --quiet
 echo Finished!
 pause
